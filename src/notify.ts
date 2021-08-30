176ed8362406d741a54e92222c08b36b38d14563
Maximilian Schneider <mail@maximilianschneider.net>
2021-08-30 19:28:41 +0200

    try to handle rate limit better

--- src/notify.ts
+++ src/notify.ts
@@ -2,7 +2,11 @@ import axios from 'axios'
 
 export default function notify(content: string) {
   if (process.env.WEBHOOK_URL) {
-    axios.post(process.env.WEBHOOK_URL, { content })
+    try {
+      axios.post(process.env.WEBHOOK_URL, { content })
+    } catch (e) {
+      console.error(`could not notify webhook: ${content}`)
+    }
   } else {
     console.warn(content)
   }
